925872cc61ac7f1b4f097202ebf417a17e283138
Arslanbek Astemirov <s-o-f-t@mail.ru>
2025-06-10 16:35:51 +0300

    feat: add monitoring reducer to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query'
 import { appReducer, appSlice } from '@/app/appSlice'
 import { authReducer, authSlice } from '@/features/auth/model/authSlice'
+import { monitoringReducer, monitoringSlice } from '@/features/monitoring/model/monitoringSlice'
 
 export const store = configureStore({
   reducer: {
     [appSlice.name]: appReducer,
     [authSlice.name]: authReducer,
+    [monitoringSlice.name]: monitoringReducer,
   },
 })
 
