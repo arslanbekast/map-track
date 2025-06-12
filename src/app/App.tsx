49c52b28cc822e985c51dd6332cb595a47038f53
Arslanbek Astemirov <s-o-f-t@mail.ru>
2025-06-12 19:47:27 +0300

    fix: change App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,18 @@
 import { Routing } from '@/common/routing/Routing'
 import { toast, ToastContainer } from 'react-toastify'
 import { useSelector } from 'react-redux'
-import { selectAppError } from '@/app/appSlice'
+import { selectAppError, setAppError } from '@/app/appSlice'
 import { useEffect } from 'react'
+import { useAppDispatch } from '@/common/hooks/useAppDispatch'
 
 export function App() {
   const error = useSelector(selectAppError)
+  const dispatch = useAppDispatch()
 
   useEffect(() => {
     if (error) {
       toast.error(error)
+      dispatch(setAppError(null))
     }
   }, [error])
   return (
