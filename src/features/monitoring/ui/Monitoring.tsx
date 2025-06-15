77be371420e56249558ac5eea817e1921cb9deb4
Arslanbek Astemirov <s-o-f-t@mail.ru>
2025-06-15 22:48:03 +0300

    refactor: refactoring Monitoring.tsx

--- src/features/monitoring/ui/Monitoring.tsx
+++ src/features/monitoring/ui/Monitoring.tsx
@@ -1,9 +1,11 @@
-import { type LatLngTuple } from 'leaflet'
 import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
 import { CircularProgress } from '@mui/material'
 import { createRandomColorIcon } from '@/common/utils/createRandomColorIcon'
 import { useMonitoring } from '@/features/monitoring/hooks/useMonitoring'
 import s from './Monitoring.module.scss'
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { selectPositions } from '@/features/monitoring/model/monitoringSlice'
 
 export const Monitoring = () => {
   const { positions, loading } = useMonitoring()
@@ -17,7 +19,7 @@ export const Monitoring = () => {
       scrollWheelZoom={true}
       className={s.mapContainer}
     >
-      <ChangeView center={positions[0].position} />
+      <ChangeView />
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -31,8 +33,11 @@ export const Monitoring = () => {
   )
 }
 
-const ChangeView = ({ center }: { center: LatLngTuple }) => {
+const ChangeView = () => {
   const map = useMap()
-  map.setView(center)
+  const positions = useSelector(selectPositions)
+  useEffect(() => {
+    map.setView(positions[0].position)
+  }, [map, positions])
   return null
 }
